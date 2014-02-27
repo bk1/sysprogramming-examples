a2f3a7334d369bec7f786ded3065f2150b4bff9f
Karl Brodowsky <bk1@gmx.net>
2014-02-27 23:09:12 +0100

    processes added#

---
+++ processes/fork-write-twice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  FILE *out = fopen("myfile", "w");
+  char *TEXT = "Beispieltext... ";
+  char *TEXTO = "Beispieltext (stdout)... ";
+  char *TEXTE = "Beispieltext (stderr)... ";
+  char *NEWLINE = "\n";
+  char *NEWLINEO = " (nl stdout)\n";
+  char *NEWLINEE = " (nl stderr)\n";
+  fwrite(TEXT, 1, strlen(TEXT), out);
+  fwrite(TEXTO, 1, strlen(TEXTO), stdout);
+  fwrite(TEXTE, 1, strlen(TEXTE), stderr);
+  fork();
+  fwrite(NEWLINE, 1, 1, out);
+  fwrite(NEWLINEO, 1, strlen(NEWLINEO), stdout);
+  fwrite(NEWLINEE, 1, strlen(NEWLINEE), stderr);
+  exit(0);
+}
