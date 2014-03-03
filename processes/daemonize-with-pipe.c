3b8e40e0d3e5fc665a9ab290343d66da69277f27
Karl Brodowsky <bk1@gmx.net>
2014-03-03 20:02:41 +0100

    improved

--- processes/daemonize-with-pipe.c
+++ processes/daemonize-with-pipe.c
@@ -19,31 +19,46 @@ int main(int argc, char *argv[]) {
   int pipes[2];
   char buff[1024];
   int ret_code;
-  char message[] = "This is the message to be transmitted";
+  /* message content is not relevant... */
+  char message[] = "X";
   printf("Started parent\n");
   ret_code = pipe(pipes);
   if (ret_code != 0) {
     printf("Error creating pipe ret_code=%d errno=%d\n", ret_code, errno);
     exit(1);
   }
+  
+  /* first fork() to create child */
   fork_result = fork();
   if (fork_result == 0) {
     printf("In child\n");
+
+    /* second fork to create grand child */
     fork_result = fork();
     if (fork_result != 0) {
+      /* exit child, make grand child a daemon */
       printf("exiting child\n");
       exit(0);
     }
+    
+    /* in grand child */
+    /* close write end of pipe */
     close(pipes[1]);
+
+    /* read from pipe */
     printf("in grandchild: waiting for pipe\n");
     ret_code = read(pipes[0], buff, strlen(message));
-    printf("received message=%s daemonized\n", message);
+    printf("grandchild has received message from grand parent: daemonized\n");
+    /* pipe has done its purpose, close it */
     close(pipes[0]);
-    printf("ppid=%d\n", getppid());
+    printf("daemon has ppid=%d\n", getppid());
+
+    /* do daemon stuff */
     sleep(30);
     printf("done with daemon\n");
     exit(0);
   }
+
   /* in parent */
   close(pipes[0]);
   int status;
