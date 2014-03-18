5a490e159e135b143da0cb2a8d3fa514d3c04d70
Karl Brodowsky <bk1@gmx.net>
2014-03-18 12:22:15 +0100

    copyright-headers

--- threads/exit-pthread-exit.c
+++ threads/exit-pthread-exit.c
@@ -1,3 +1,10 @@
+/* (C) IT Sky Consulting GmbH 2014
+ * http://www.it-sky-consulting.com/
+ * Author: Karl Brodowsky
+ * Date: 2014-02-27
+ * License: GPL v2 (See https://de.wikipedia.org/wiki/GNU_General_Public_License )
+ */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
