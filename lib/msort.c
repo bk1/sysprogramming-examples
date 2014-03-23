87a6ee0c0eee419fe2e6e32d3b34c5814fe58746
Karl Brodowsky <bk1@gmx.net>
2014-03-23 03:54:06 +0100

    test renamed, prints removed

--- lib/msort.c
+++ lib/msort.c
@@ -34,17 +34,11 @@ void msort_r(void *base,
     return;
   }
 
-  // printf("msort_r\n");
 
   void *scratch = malloc(nmemb * size);
 
   for (size_t step = 1; step < nmemb; step *= 2) {
     size_t dstep = 2*step;
-    // printf("step=%ld dstep=%ld [", step, dstep);
-    // for (size_t qq = 0; qq < nmemb; qq++) {
-      // printf(" %d ", ((int *)base)[qq]);
-    // }
-    // printf("]\n");
     size_t start_left;
     size_t start_right;
     for (start_left = 0, start_right = step; start_right < nmemb; start_left += dstep, start_right += dstep) {
@@ -56,87 +50,32 @@ void msort_r(void *base,
         n = nmemb - start_right;
       }
       size_t total_len = m + n;
-      // printf("start_left=%ld start_right=%ld nmemb=%ld m=%ld n=%ld total_len=%ld [", start_left, start_right, nmemb, m, n, total_len);
-      // for (size_t qq = 0; qq < nmemb; qq++) {
-        // printf(" %d ", ((int *)base)[qq]);
-      // }
-      // printf("]\n");
       void *result = scratch;
       void *left  = POINTER(base, start_left,  size);
       void *right = POINTER(base, start_right, size);
       void *source = left;
       while (i+j < total_len) {
-        // printf("i=%ld j=%ld m=%ld n=%ld src=[", i, j, m, n);
-        // for (size_t qq = 0; qq < nmemb; qq++) {
-          // printf(" %d ", ((int *)base)[qq]);
-        // }
-        // printf("] dest=[");
-        // for (size_t qq = 0; qq < i+j; qq++) {
-          // printf(" %d ", ((int *)scratch)[qq]);
-        // }
-        // printf("]\n");
         if (i >= m) {
           memcpy(result, right, (n-j)*size);
           j = n;
-          // printf("was i>=m (copied rest of right) i=%ld j=%ld m=%ld n=%ld src=[", i, j, m, n);
-          // for (size_t qq = 0; qq < nmemb; qq++) {
-            // printf(" %d ", ((int *)base)[qq]);
-          // }
-          // printf("] dest=[");
-          // for (size_t qq = 0; qq < i+j; qq++) {
-            // printf(" %d ", ((int *)scratch)[qq]);
-          // }
-          // printf("]\n");
         } else if (j >= n) {
           memcpy(result, left, (m-i)*size);
           i = m;
-          // printf("was j>=n (copied rest of left) i=%ld j=%ld m=%ld n=%ld src=[", i, j, m, n);
-          // for (size_t qq = 0; qq < nmemb; qq++) {
-            // printf(" %d ", ((int *)base)[qq]);
-          // }
-          // printf("] dest=[");
-          // for (size_t qq = 0; qq < i+j; qq++) {
-            // printf(" %d ", ((int *)scratch)[qq]);
-          // }
-          // printf("]\n");
         } else {
           if (compare(left, right, arg) <= 0) {
             memcpy(result, left, size);
             result += size;
             left += size;
             i++;
-            // printf("was left<=right (copied one from left) i=%ld j=%ld m=%ld n=%ld src=[", i, j, m, n);
-            // for (size_t qq = 0; qq < nmemb; qq++) {
-              // printf(" %d ", ((int *)base)[qq]);
-            // }
-            // printf("] dest=[");
-            // for (size_t qq = 0; qq < i+j; qq++) {
-              // printf(" %d ", ((int *)scratch)[qq]);
-            // }
-            // printf("]\n");
           } else {
             memcpy(result, right, size);
             result += size;
             right += size;
             j++;
-            // printf("was left > right (copied one from right) i=%ld j=%ld m=%ld n=%ld src=[", i, j, m, n);
-            // for (size_t qq = 0; qq < nmemb; qq++) {
-              // printf(" %d ", ((int *)base)[qq]);
-            // }
-            // printf("] dest=[");
-            // for (size_t qq = 0; qq < i+j; qq++) {
-              // printf(" %d ", ((int *)scratch)[qq]);
-            // }
-            // printf("]\n");
           }
         }
       }
       memcpy(source, scratch, total_len*size);
-      // printf("i=%ld j=%ld m=%ld n=%ld total_len=%ld [", i, j, m, n, total_len);
-      // for (size_t qq = 0; qq < nmemb; qq++) {
-        // printf(" %d ", ((int *)base)[qq]);
-      // }
-      // printf("]\n");
     }
   }
 }
