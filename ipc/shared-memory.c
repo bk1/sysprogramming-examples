677870cc7dcfc08f5fe45b7fdd95484288f9540a
Karl Brodowsky <bk1@gmx.net>
2014-03-25 17:28:19 +0100

    ipc/shared-memory.c now working

--- ipc/shared-memory.c
+++ ipc/shared-memory.c
@@ -5,7 +5,6 @@
  * License: GPL v2 (See https://de.wikipedia.org/wiki/GNU_General_Public_License )
  */
 
-/* WARNING: does not work currently */
 /* start a worker thread.  Put assignments for worker thread into shared memory.  Extract result from there and print it */
 
 #include <stdio.h>
@@ -32,13 +31,13 @@ int shmid_for_cleanup = 0;
 int ready = FALSE;
 
 struct data {
-  int ppid;
-  int cpid;
-  int pready;
-  int cready;
-  long x;
-  long y;
-  char cmd;
+  int ppid; /* parent pid */
+  int cpid; /* child pid (worker) */
+  int pready; /* child waits for pready before providing result */
+  int cready; /* parent waits for cready before providing next request */
+  long x; /* value for request */
+  long y; /* result */
+  char cmd; /* command provided by parent to worker=child: Q for quit, "*" for calculating x*x */
 };
 
 const int SIZE = sizeof(struct data);
@@ -48,7 +47,7 @@ const char *REF_FILE = "./shmref.dat";
 int create_shm(key_t key, const char *txt, const char *etxt) {
   int shm_id = shmget(key, SIZE, IPC_CREAT | PERM);
   handle_error(shm_id, etxt, PROCESS_EXIT);
-  printf("%s: shm_id=%d key=%ld\n", txt, shm_id, (long) key);
+  // printf("%s: shm_id=%d key=%ld\n", txt, shm_id, (long) key);
   return shm_id;
 }
 
@@ -63,7 +62,7 @@ void cleanup() {
 
 void my_handler(int signo) {
   if (signo == SIGUSR1) {
-    printf("received SIGUSR1\n");
+    // printf("received SIGUSR1\n");
     ready = TRUE;
   } else {
     cleanup();
@@ -78,51 +77,55 @@ void show_shm_ctl(int shm_id, const char *txt) {
   struct shmid_ds shmctl_data;
   retcode = shmctl(shm_id, IPC_STAT, &shmctl_data);
   handle_error(retcode, "child shmctl failed", PROCESS_EXIT);
-  struct ipc_perm perms = shmctl_data.shm_perm;
-  printf("%s: key=%ld uid=%d gid=%d cuid=%d cgid=%d mode=%d seq=%d\n", txt, (long) perms.__key, (int) perms.uid, (int) perms.gid, (int) perms.cuid, (int) perms.cgid, (int) perms.mode, (int)perms.__seq);
+  // struct ipc_perm perms = shmctl_data.shm_perm;
+  // printf("%s: key=%ld uid=%d gid=%d cuid=%d cgid=%d mode=%d seq=%d\n", txt, (long) perms.__key, (int) perms.uid, (int) perms.gid, (int) perms.cuid, (int) perms.cgid, (int) perms.mode, (int)perms.__seq);
 }
 
 void parent(key_t shm_key, pid_t child_pid, sigset_t *suspendset_ptr) {
   int retcode;
 
   atexit(cleanup);
-  printf("in parent child_pid=%d\n", getpid());
+  printf("in parent pid=%d\n", getpid());
   int shm_id = create_shm(shm_key, "worker", "worker shmget failed");
   struct data *shm_data = (struct data *) shmat(shm_id, NULL, 0);
-  printf("parent: address=%ld\n", (long) shm_data);
+  // printf("parent: address=%ld\n", (long) shm_data);
   shm_data->ppid = getpid();
-  shm_data->pready = FALSE;
-  for (int i = 0; i < 5; i++) {
-    printf("P: p=%d c=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->x, shm_data->y, shm_data->cmd);
-    sleep(1);
-  }
-  while (! ready) {
-    retcode = sigsuspend(suspendset_ptr);
-    handle_error(retcode, "parent: sigsuspend", PROCESS_EXIT);
-  }
-  ready = FALSE;
+  // printf("P: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
+
   for (int i = 0; i < 100; i++) {
+
+    /* wait for the child to be ready */
+    while (! shm_data->cready) {
+      // printf("P: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
+      retcode = sigsuspend(suspendset_ptr);
+      if (errno != EINTR) {
+        handle_error(retcode, "parent: sigsuspend", PROCESS_EXIT);
+      }
+    }
+    // printf("P: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
+
+    shm_data->cready = FALSE;
     shm_data->x = i;
     shm_data->cmd = '*';
     shm_data->pready = TRUE;
     printf("P: x=%3ld y=%6ld\n", shm_data->x, shm_data->y);
-    printf("parent sending signal to worker\n");
-    retcode = kill(SIGUSR1, child_pid);
+    // printf("parent sending signal to worker\n");
+    retcode = kill(child_pid, SIGUSR1);
     handle_error(retcode, "parent: kill", PROCESS_EXIT);
-    while (! shm_data->cready) {
-      sigsuspend(suspendset_ptr);
-    }
-    shm_data->pready = FALSE;
-    ready = FALSE;
-    printf("P: x=%3ld y=%6ld\n", shm_data->x, shm_data->y);
+    // printf("P: x=%3ld y=%6ld\n", shm_data->x, shm_data->y);
+    // printf("P: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
   }
+
+  shm_data->cready = FALSE;
   shm_data->cmd = 'Q';
-  retcode = kill(SIGUSR1, child_pid);
+  shm_data->pready = TRUE;
+  retcode = kill(child_pid, SIGUSR1);
   handle_error(retcode, "child: kill", PROCESS_EXIT);
   int status;
   wait(&status);
   printf("child terminated->terminating parent\n");
   shmdt(shm_data);
+  shm_data = NULL; /* avoid using the pointer after it has been disabled */
   printf("terminating parent: DONE\n");
 }
 
@@ -133,34 +136,36 @@ void child(key_t shm_key, sigset_t *suspendset_ptr) {
 
   int shm_id = create_shm(shm_key, "worker", "worker shmget failed");
   struct data *shm_data = (struct data *) shmat(shm_id, NULL, 0);
-  printf("worker: address=%ld\n", (long) shm_data);
+  // printf("worker: address=%ld\n", (long) shm_data);
   shm_data->cpid = getpid();
   shm_data->cready = TRUE;
-  for (int i = 0; i < 5; i++) {
-    printf("W: p=%d c=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->x, shm_data->y, shm_data->cmd);
-    sleep(1);
-  }
+  // printf("W: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
+
   pid_t ppid = getppid();
-  printf("child sending signal to ppid=%ld indicate readiness\n", (long) ppid);
-  kill(SIGUSR1, ppid);
+  // printf("child sending signal to ppid=%ld indicate readiness\n", (long) ppid);
+  kill(ppid, SIGUSR1);
   while (TRUE) {
     while (! shm_data->pready) {
       // retcode = pause();
       // handle_error(retcode, "pause", PROCESS_EXIT);
       retcode = sigsuspend(suspendset_ptr);
-      printf("returned retcode=%d errno=%d\n", retcode, errno);
+      if (errno != EINTR) {
+        handle_error(retcode, "child: sigsuspend", PROCESS_EXIT);
+      }
     }
-    shm_data->cready = FALSE;
-    ready = FALSE;
+    // printf("W: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
+    shm_data->pready = FALSE;
     if (shm_data->cmd == 'Q') {
       shmdt(shm_data);
+      shm_data = NULL; /* avoid using the pointer after it has been disabled */
       printf("terminating worker: DONE\n");
       exit(0);
     }
     shm_data->y = shm_data->x * shm_data->x;
     shm_data->cready = TRUE;
-    printf("worker sending signal to parent\n");
-    kill(SIGUSR1, shm_data->ppid);
+    // printf("W: p=%d c=%d pready=%d cready=%d x=%ld y=%ld c=%c\n", shm_data->ppid, shm_data->cpid, shm_data->pready, shm_data->cready, shm_data->x, shm_data->y, shm_data->cmd);
+    // printf("worker sending signal to parent\n");
+    kill(shm_data->ppid, SIGUSR1);
   }
 }
 
@@ -200,6 +205,12 @@ int main(int argc, char *argv[]) {
   if (shm_key < 0) {
     handle_error(-1, "ftok failed", PROCESS_EXIT);
   }
+  int shm_id = create_shm(shm_key, "main", "main shmget failed");
+  struct data *shm_data = (struct data *) shmat(shm_id, NULL, 0);
+  shm_data->cready = FALSE;
+  shm_data->pready = FALSE;
+  shmdt(shm_data);
+  shm_data = NULL; /* avoid using the pointer after it has been disabled */
 
   /* create a worker process */
   pid_t child_pid = fork();
