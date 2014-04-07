4b99332ae8d3336aa7d5dd430a63d388120b7547
Karl Brodowsky <bk1@gmx.net>
2014-04-07 12:58:59 +0200

    added example with DNS

--- posix/socket/tcp-square-client-dns.c
+++ posix/socket/tcp-square-client-dns.c
@@ -7,8 +7,11 @@
  * This file is inspired by http://cs.baylor.edu/~donahoo/practical/CSockets/code/HandleTCPClient.c
  */
 
+// TODO use getaddrinfo()
+
 #include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
 #include <errno.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <stdio.h>
@@ -19,17 +22,29 @@
 #include <sys/types.h>
 #include <unistd.h>     /* for close() */
 
+//       int getaddrinfo(const char *node, const char *service,
+//                       const struct addrinfo *hints,
+//                       struct addrinfo **res);
+
+//       void freeaddrinfo(struct addrinfo *res);
+
+//       const char *gai_strerror(int errcode);
+
 #include <itskylib.h>
 #include <transmission-protocols.h>
 
 #define RCVBUFSIZE 32   /* Size of receive buffer */
+
 
 void usage(const char *argv0, const char *msg) {
   if (msg != NULL && strlen(msg) > 0) {
     printf("%s\n\n", msg);
   }
   printf("Usage\n\n");
-  printf("%s <Server IP> [<Port>]\n", argv0);
+  printf("%s <Server (hostname)> [<Port>] [<IPVersion>]\n", argv0);
+  printf("1st argument is the name of the server\n");
+  printf("2nd argument is the port.  Optional, defaults to 7000\n");
+  printf("3rd argument is the Version of IP (4 or 6).  Optional, defaults to 4 (IPv4)\n");
   printf("runs in an infinite loop reading input and sending it to the server and printing the result\n");
   printf("end with Ctrl-C or QUIT\n");
   exit(1);
@@ -43,36 +58,67 @@ int main(int argc, char *argv[]) {
     usage(argv[0], "");
   }
 
-  int sock;                        /* Socket descriptor */
-  struct sockaddr_in server_address; /* Square server address */
-  unsigned short server_port;     /* Square server port */
-  char *server_ip;                    /* Server IP address (dotted quad) */
-
-  if (argc < 2 || argc > 3) {    /* Test for correct number of arguments */
+  if (argc < 2 || argc > 4) {    /* Test for correct number of arguments */
     usage(argv[0], "wrong number of arguments");
   }
 
-  server_ip = argv[1];             /* First arg: server IP address (dotted quad) */
+  char *server_name = argv[1];             /* First arg: server IP address (dotted quad) */
 
-  if (argc == 3) {
-    server_port = atoi(argv[2]); /* Use given port, if any */
+  char service_or_server_port[1024];
+  if (argc >= 3) {
+    sprintf(service_or_server_port, "%s", argv[2]);
   } else {
-    server_port = 7000;  /* 7000 is a free port */
+    sprintf(service_or_server_port, "7000");
+  }
+
+  int ip_version = -1;
+  if (argc >= 4) {
+    ip_version = atoi(argv[3]);
+  } else {
+    ip_version = 4;
+  }
+
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  if (ip_version == 4) {
+    hints.ai_family = AF_INET;
+  } else if (ip_version == 6) {
+    hints.ai_family = AF_INET6;
+  }
+  hints.ai_socktype = SOCK_STREAM;
+  struct addrinfo *result;
+
+  retcode = getaddrinfo(server_name, service_or_server_port, &hints, &result);
+  handle_error(retcode, "getaddrinfo() failed", PROCESS_EXIT);
+  if (result->ai_family == AF_INET6) {
+    ip_version = 6;
+  } else {
+    ip_version = 4;
   }
 
   /* Create a reliable, stream socket using TCP */
-  sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  handle_error(sock, "socket() failed", PROCESS_EXIT);
+  struct addrinfo *rp;
+  int sock = -1;                        /* Socket descriptor */
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (sock == -1) {
+      // look errno
+      //   handle_error(sock, "socket() failed", PROCESS_EXIT);
 
-  /* Construct the server address structure */
-  memset(&server_address, 0, sizeof(server_address));     /* Zero out structure */
-  server_address.sin_family      = AF_INET;             /* Internet address family */
-  server_address.sin_addr.s_addr = inet_addr(server_ip);   /* Server IP address */
-  server_address.sin_port        = htons(server_port); /* Server port: htons host to network byte order */
-
-  /* Establish the connection to the square server */
-  retcode = connect(sock, (struct sockaddr *) &server_address, sizeof(server_address));
-  handle_error(retcode, "connect() failed", PROCESS_EXIT);
+      continue;
+    }
+    /* Establish the connection to the square server */
+    retcode = connect(sock, rp->ai_addr, rp->ai_addrlen);
+    if (retcode == 0) {
+      break;                  /* Success */
+    }
+    // look errno
+    // retcode = connect(sock, (struct sockaddr *) &server_address, sizeof(server_address));
+    // handle_error(retcode, "connect() failed", PROCESS_EXIT);
+    close(sock);
+    sock = -1;
+  }
+  handle_error(sock, "no connection could be established", PROCESS_EXIT);
 
   char *buffer_ptr[1];
 
