73ed71d33d946be162ed6942fb4e134b4301e90d
Karl Brodowsky <bk1@gmx.net>
2014-05-06 10:16:09 +0200

    new file added for binary io

---
+++ win32/io/fopen-fclose-bin.c
@@ -0,0 +1,18 @@
+/* (C) IT Sky Consulting GmbH 2014
+ * http://www.it-sky-consulting.com/
+ * Author: Karl Brodowsky
+ * Date: 2014-02-27
+ * License: GPL v2 (See https://de.wikipedia.org/wiki/GNU_General_Public_License )
+ *
+ * lowlevel IO compiles, but crashes
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  FILE *out = fopen("../temp/test-hb.txt", "wb");
+  const char *msg = "Hello, World!\n(fopen/fwrite/fclose)";
+  fwrite(msg, 1, strlen(msg), out);
+  fclose(out);
+}
