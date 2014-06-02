ec507529573d7fa7cd507d51d2747686f5d8737a
Karl Brodowsky <bk1@gmx.net>
2014-06-02 21:34:03 +0200

    fixed minor stuff

--- posix/adv-io/select-input.c
+++ posix/adv-io/select-input.c
@@ -13,32 +13,37 @@
 
 int main(int argc, char **argv) {
   int retcode;
-  if (argc < 3) {
-    printf("USAGE:\n\n%s buffersize inpipe1 inpipe2 ... inpipen\n", argv[0]);
+  if (argc < 5) {
+    printf("USAGE:\n\n%s buffersize sec usec inpipe1 inpipe2 ... inpipen\n", argv[0]);
     exit(1);
   }
 
   int buffer_size = atoi(argv[1]);
   char *buffer = malloc(buffer_size + 1);
-
-  int nfds = argc-2;
+  int sec = atoi(argv[2]);
+  int nsec = atoi(argv[3]);
+  int nfds = argc-4;
 
   int fdin[nfds];
   fd_set fdin_set;
   FD_ZERO(&fdin_set);
   for (int i = 0; i < nfds; i++) {
-    printf("opening fdin[%d] for %s\n", i, argv[2+i]);
-    fdin[i] = open(argv[2+i], O_RDONLY | O_NONBLOCK);
+    printf("opening fdin[%d] for %s\n", i, argv[4+i]);
+    fdin[i] = open(argv[4+i], O_RDONLY | O_NONBLOCK);
     handle_error(fdin[i], "open", PROCESS_EXIT);
     FD_SET(fdin[i], &fdin_set);
   }
-
-  struct timespec timeout;
-  timeout.tv_sec = 10;
-  timeout.tv_nsec = 0;
+  if (sec >= 0 && nsec >= 0) {
+  struct timeval timeout;
+  timeout.tv_sec = sec;
+  timeout.tv_usec = nsec;
   retcode = select(nfds, &fdin_set, NULL, NULL, &timeout);
+  } else {
+  retcode = select(nfds, &fdin_set, NULL, NULL, NULL);
+
+  }
   handle_error(retcode, "select", PROCESS_EXIT);
-  printf("found entries for %d descriptors", retcode);
+  printf("found entries for %d descriptors\n", retcode);
   for (int i = 0; i < nfds; i++) {
     if (FD_ISSET(fdin[i], &fdin_set)) {
       printf("found stuff for i=%d fd=%d\n", i, fdin[i]);
