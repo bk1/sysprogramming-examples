87971f4c6eb1856cc91d69553f8f7b9b7848d722
Karl Brodowsky <bk1@gmx.net>
2015-06-02 01:28:53 +0200

    fixed async open & read

--- posix/adv-io/async-io-multi-pipe.c
+++ posix/adv-io/async-io-multi-pipe.c
@@ -16,9 +16,13 @@
 #include <string.h>
 #include <aio.h>
 
+#include <pthread.h>
+
 #include <itskylib.h>
 
 int buffer_size;
+int nfds;
+char **pipe_names;
 
 void usage(char *msg, char *argv0) {
   if (msg != NULL && strlen(msg) > 0) {
@@ -39,11 +43,17 @@ void *handle_pipe_data(void *arg) {
   handle_error(retcode, "aio_error", THREAD_EXIT);
   ssize_t size_read = aio_return(aiocbp);
   handle_error(retcode, "aio_return", THREAD_EXIT);
+
+  int fd = aiocbp->aio_fildes;
+  char *name = pipe_names[fd];
+  if (size_read == 0) {
+    printf("EOF for %s (fd=%d)\n", name, fd);
+    return NULL;
+  }
   ((char *)aiocbp->aio_buf)[size_read] = '\000';
-  printf("read %ld bytes from fd=%d:\n%s\n", (long) size_read, aiocbp->aio_fildes, (char *) aiocbp->aio_buf);
-  sleep(1);
+  printf("read %ld bytes from %s (fd=%d):\n%s\n", (long) size_read, name, fd, (char *) aiocbp->aio_buf);
+  sleep(10);
   exit(0);
-  return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -55,27 +65,49 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
+  int max_fd = -1;
   int retcode;
 
   buffer_size = atoi(argv[1]);
-  int nfds = argc-2;
+  int npipes = argc-2;
 
-  struct aiocb *aiocbs = (struct aiocb *) malloc(nfds * sizeof(struct aiocb));
+  struct aiocb *aiocbs = (struct aiocb *) malloc(npipes * sizeof(struct aiocb));
 
   struct sigevent sigevent;
   memset(&sigevent, 0, sizeof(sigevent));
   sigevent.sigev_notify = SIGEV_THREAD;
   sigevent.sigev_notify_function = handle_pipe_data;
-  
-  for (int i = 0; i < nfds; i++) {
-    char *name = argv[2+i];
-    int fd = open(name, O_RDONLY | O_NONBLOCK);
 
+  int *fds = (int *) malloc(npipes * sizeof(int));
+  for (int i = 0; i < npipes; i++) {
+    char *name = argv[2+i];
+
+    // O_NONBLOCK makes the open on a pipe succeed, even if there is no writer open on the other side:
+    int fd = open(name, O_RDONLY | O_NONBLOCK);
     handle_error(fd, "open", PROCESS_EXIT);
+    if (fd > max_fd) {
+      max_fd = fd;
+    }
+    fds[i] = fd;
+  }
+  nfds = max_fd + 1;
+  pipe_names = (char **) malloc(nfds * sizeof(char *));
+  for (int i = 0; i < nfds; i++) {
+    pipe_names[i] = "";
+  }
+  for (int i = 0; i < npipes; i++) {
+    pipe_names[fds[i]] = argv[2+i];
+  }
+
+  for (int i = 0; i < npipes; i++) {
+    int fd = fds[i];
+
+    // set it to BLOCKING:
     int flags = fcntl(fd, F_GETFL, 0);
     handle_error(flags, "fcntl F_GETFL", PROCESS_EXIT);
     flags &= ~O_NONBLOCK;
     retcode = fcntl(fd, F_SETFL, flags);
+    handle_error(retcode, "fcntl F_SETFL", PROCESS_EXIT);
 
     memset(&(aiocbs[i]), 0, sizeof(struct aiocb));
     sigevent.sigev_value.sival_ptr = &aiocbs[i];
@@ -84,11 +116,12 @@ int main(int argc, char **argv) {
     aiocbs[i].aio_buf    = malloc(buffer_size + 1);
     aiocbs[i].aio_nbytes = buffer_size;
     aiocbs[i].aio_sigevent = sigevent;
+    aiocbs[i].aio_lio_opcode = LIO_READ;
     retcode = aio_read(&aiocbs[i]);
     handle_error(retcode, "aio_read", PROCESS_EXIT);
   }
   /* let the child threads do the work */
   printf("all assync reads started\n");
-  sleep(120);
+  pthread_exit(NULL);
   exit(0);
 }
